1406482785dee5c4bc4ad6986b0626794b72fba2
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-20 17:56:05 +0200

    🍱 products images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
-import { CommonModule } from './common/common.module';
+
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { CommonModule } from './common/common.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -19,6 +20,9 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
     ProductsModule,
     CommonModule,
     SeedModule,
