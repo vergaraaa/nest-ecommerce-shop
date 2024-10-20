e455c70dd7275fec3cb3cbec43980b872ad43345
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-20 07:59:57 +0200

    ✨ refine findOne function

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -41,11 +42,23 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('UPPER(title)=:title or slug=:slug', {
+          title: term.toUpperCase(),
+          slug: term.toLowerCase(),
+        })
+        .getOne();
+    }
 
     if (!product)
-      throw new NotFoundException(`Product with id '${id}' not found`);
+      throw new NotFoundException(`Product with term '${term}' not found`);
 
     return product;
   }
